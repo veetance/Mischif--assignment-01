7930ce542394c60e7a9160500d1486f9c1bc9d53
veetance <divineokos@hotmail.com>
2022-11-06 01:24:35 -0600

    Landing Ui Complete- side panel added

--- www/js/mr-rager-app.js
+++ www/js/mr-rager-app.js
@@ -1,26 +1,20 @@
 var app = new Framework7({
   // App root element
-  el: '#app',
+  el: "#app",
   // other parameters
 
   routes: [
-
     {
-      path: '/' ,
-      url: 'index.html' ,
-    },
-    
-    {
-      path: '/page3/',
-      url: 'pages/page3.html',
+      path: "/",
+      url: "index.html",
     },
 
+    {
+      path: "/page3/",
+      url: "pages/page3.html",
+    },
   ],
-
 });
 
-var mainView = app.views.create('.view-main')
-
-
-
+var mainView = app.views.create(".view-main");
 
