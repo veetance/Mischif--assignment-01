3bb83c23af41b883d6ba55535c0d484e70df4639
veetance <divineokos@hotmail.com>
2022-11-09 20:12:48 -0700

    Side panel background load/clone and dump complete

--- www/js/mr-rager-app.js
+++ www/js/mr-rager-app.js
@@ -21,8 +21,7 @@ var mainView = app.views.create(".view-main");
 
 
 
-//  using jQuery create an array loop of 6 images from local folder and populate .background-wrapper with images that have a class of .background-thumbnail thumbnail
-
+//  using jQuery create an array loop of 6 images from local folder and populate.background-wrapper with images that have a class of .background-thumbnail thumbnail
 var bkgimages = [
   "../www/img/bkg-folder/bkg-01.png",
   "../www/img/bkg-folder/bkg-02.png",
@@ -35,7 +34,7 @@ var bkgimages = [
   for (var i = 0; i < bkgimages.length; i++) {
     var img = $('<img />', {
       src: bkgimages[i],
-      class: "background-thumbnail thumbnail",
+      class: "background-thumbnail thumbnail panel-close ",
       id: "bkg-" + i,
     });
     $(".background-wrapper").append(img);
@@ -45,7 +44,68 @@ var bkgimages = [
 
 
 
-// using j
+//event listner function that clones .background-thumbnail onclick and appends it to .canvas-wrapper if the .canvas-wrapper has more than 1 image then remove the first image in the .canvas-wrapper and remove .thumbnail class from cloned image
+$(function() { 
+  $(".background-wrapper").on("click", ".background-thumbnail", function() {
+    var clone = $(this).clone();
+    $(".canvas-wrapper").append(clone);
+    if ($(".canvas-wrapper").children().length > 1) {
+      $(".canvas-wrapper").children().first().remove();
+    }
+    clone.removeClass("thumbnail");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
